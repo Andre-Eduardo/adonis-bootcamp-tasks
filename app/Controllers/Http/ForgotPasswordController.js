679a072a69c822b7f19282af53108319cc246280
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-06-04 16:26:59 -0300

    send email

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto');
 const User = use('App/Models/User');
+const Mail = use('Mail');
 
 class ForgotPasswordController {
   async store({ request, response }) {
@@ -12,6 +13,21 @@ class ForgotPasswordController {
       user.token = crypto.randomBytes(10).toString('hex');
       user.token_created_at = new Date();
       await user.save();
+
+      await Mail.send(
+        ['email.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`,
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('andreeduardo@hotmail.com', 'Andre Eduardo')
+            .subject('Recuperação de senha');
+        }
+      );
     } catch (err) {
       return response.status(err.status).send({
         error: { message: 'Algo nao deu certo, esse e-mail existe?' },
