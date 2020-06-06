35d3f6b4c13f245b925811a9b846ac7009ef69d7
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-06-05 22:50:05 -0300

    add internacionalization

--- app/Validators/ResetPassword.js
+++ app/Validators/ResetPassword.js
@@ -1,4 +1,5 @@
 'use strict';
+const Antl = use('Antl');
 
 class ResetPassword {
   get validateAll() {
@@ -10,6 +11,9 @@ class ResetPassword {
       token: 'requied',
       password: 'requied|confirmed',
     };
+  }
+  get messages() {
+    return Antl.list('validation');
   }
 }
 
